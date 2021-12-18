6b15e78e1aeea6278e8c19b44cdf280c50681bbb
afeiship <1290657123@qq.com>
2021-12-18 23:41:28 +0800

    chore: add weiboUploadMediaAdapter demo

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 import ReactDemokit from '@jswork/react-demokit';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import weiboUploadMediaAdapter from '@jswork/weibo-upload-media-adapter';
 import ReactUploadMedia from '../src/main';
 import './assets/style.scss';
 
@@ -8,7 +9,7 @@ import '@jswork/next-times';
 
 class App extends React.Component {
   state = {
-    value2:[],
+    value2: [],
     value: [
       'https://tva1.sinaimg.cn/large/007S8ZIlgy1gexw87htqhj305k05k74o.jpg',
       'https://tva1.sinaimg.cn/large/008i3skNgy1gqh868msafj302s02st8l.jpg',
@@ -17,19 +18,20 @@ class App extends React.Component {
       'https://randomuser.me/api/portraits/lego/3.jpg',
       'https://randomuser.me/api/portraits/lego/4.jpg',
       'https://randomuser.me/api/portraits/lego/5.jpg',
-      'https://randomuser.me/api/portraits/lego/6.jpg',
+      'https://randomuser.me/api/portraits/lego/6.jpg'
     ]
   };
 
   handleChange = (e) => {
-    console.log('handle change:',e.target.value);
+    console.log('handle change:', e.target.value);
     this.setState({ value: e.target.value });
   };
 
   handleUpload = (e) => {
-    console.log('loading...');
+    console.log('loading...', e);
     const values = e.target.value;
     console.log('values:', values);
+    // return weiboUploadMediaAdapter(e);
     return new Promise((resolve) => {
       setTimeout(() => {
         const urls = nx.times(
@@ -43,16 +45,16 @@ class App extends React.Component {
     // console.log('upload info:', e.target.value);
   };
 
-  render() {
+  render () {
     return (
       <ReactDemokit
-        className="p-3 app-container"
-        url="https://github.com/afeiship/react-upload-media">
+        className='p-3 app-container'
+        url='https://github.com/afeiship/react-upload-media'>
         <ReactUploadMedia
           count={4}
           value={this.state.value}
           onChange={this.handleChange}
-          onUpload={this.handleUpload}
+          onUpload={weiboUploadMediaAdapter}
         />
       </ReactDemokit>
     );
